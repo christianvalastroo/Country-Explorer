0ff6329254249ffd665351272ce0249a9ee0a7b9
Christian Valastro <christianvalastro@icloud.com>
2026-05-07 19:31:57 +0200

    Configura struttura iniziale dell'app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap"
+
+const App = () => {
+  return (
+    <Container className="my-5">
+      <h1 className="text-center mb-4">Country Exsplorer 🌍</h1>
+    </Container>
+  )
+}
+
+export default App
